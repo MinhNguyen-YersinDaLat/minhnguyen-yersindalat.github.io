322adae3e94ac8708df6c9024205891dcf920a64
MinhNguyen-YersinDaLat <thaiminhnguyendlu@gmail.com>
2025-09-06 19:04:08 +0700

    Delete gamephongchongthuocla directory

--- gamephongchongthuocla/preload.js
+++
@@ -1,8 +0,0 @@
-// preload.js
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  quitApp: () => ipcRenderer.send('quit-app'),
-  // nếu cần lắng nghe event từ main
-  onMainMessage: (cb) => ipcRenderer.on('some-main-event', cb)
-});
